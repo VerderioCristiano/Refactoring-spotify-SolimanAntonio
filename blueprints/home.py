e6962ae65486c4b3c972b5f81fb756c91514c55d
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2025-02-25 15:17:22 +0000

    python autenticazione finito # Please enter the commit message for your changes. Lines starting

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect, url_for, request
-from services.spotify_oauth import get_spotify_client
+from flask import Blueprint, render_template, session
+from blueprints.authf import get_spotify_client
 import spotipy
 
 home_bp = Blueprint("home", __name__)
@@ -7,36 +7,37 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def home():
     sp = get_spotify_client()
-    if not isinstance(sp, spotipy.Spotify):
-        return sp  
 
+    user_info, playlists = None, []
     try:
-        user_info = sp.current_user()
-        playlists = sp.current_user_playlists()["items"]
+        if isinstance(sp, spotipy.Spotify):  # Controllo se ho un client valido
+            user_info = sp.current_user()
+            playlists = sp.current_user_playlists()["items"]
     except Exception:
-        return redirect(url_for("auth.login"))
+        pass  # Se l'utente non è autenticato, non mostriamo nulla
 
     return render_template("home.html", user_info=user_info, playlists=playlists)
+
 
 @home_bp.route("/playlist/<playlist_id>")
 def playlist(playlist_id):
     sp = get_spotify_client()
-    if not isinstance(sp, spotipy.Spotify):
-        return sp  
-    
-    try:
-        playlist_data = sp.playlist(playlist_id)
-        tracks_data = playlist_data["tracks"]["items"]
-        tracks = [
-            {
-                "name": track["track"]["name"],
-                "artist": track["track"]["artists"][0]["name"],
-                "album": track["track"]["album"]["name"],
-                "cover": track["track"]["album"].get("images", [{}])[0].get("url")
-            }
-            for track in tracks_data if track.get("track")
-        ]
-    except Exception:
-        return redirect(url_for("auth.login"))
 
-    return render_template("playlist.html", tracks=tracks)
+    playlist_data, tracks = None, []
+    try:
+        if isinstance(sp, spotipy.Spotify):
+            playlist_data = sp.playlist(playlist_id)
+            tracks_data = playlist_data["tracks"]["items"]
+            tracks = [
+                {
+                    "name": track["track"]["name"],
+                    "artist": track["track"]["artists"][0]["name"],
+                    "album": track["track"]["album"]["name"],
+                    "cover": track["track"]["album"].get("images", [{}])[0].get("url")
+                }
+                for track in tracks_data if track.get("track")
+            ]
+    except Exception:
+        pass  # Se c'è un errore, lasciamo la pagina vuota o con un messaggio
+
+    return render_template("playlist.html", playlist=playlist_data, tracks=tracks)
